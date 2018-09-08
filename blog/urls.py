7d813ab0d46b85fb116adac35da0c35c1abc0889
Haidara <ababacarhaidar@gmail.com>
2018-09-08 12:54:38 +0000

    Des formulaire sans les modèles

--- blog/urls.py
+++ blog/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('article/<int:id>-<slug:slug>', views.lire, name='lire'),
     path('redirection',views.view_redirection,name="rd"),
     path('date', views.date_actuelle),
-    path('addition/<int:nombre1>/<int:nombre2>/', views.addition)
+    path('addition/<int:nombre1>/<int:nombre2>/', views.addition),
+    path('contact/', views.contact, name='contact'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render,redirect,get_object_or_404
 from django.http import HttpResponse,Http404
 from datetime import datetime
 from django.http import Http404
+
+from .forms import ContactForm
 from blog.models import Article
 # Create your views here.
 
@@ -36,3 +38,26 @@ def accueil(request):
 def lire(request, id,slug):
     article = get_object_or_404(Article, id=id,slug=slug)
     return render(request, 'blog/lire.html', {'article':article})
+
+# Les formulaires 
+def contact(request):
+    # Construire le formulaire, soit avec les données postées,
+    # soit vide si l'utilisateur accède pour la première fois
+    # à la page.
+    form = ContactForm(request.POST or None)
+    # Nous vérifions que les données envoyées sont valides
+    # Cette méthode renvoie False s'il n'y a pas de données 
+    # dans le formulaire ou qu'il contient des erreurs.
+    if form.is_valid(): 
+        # Ici nous pouvons traiter les données du formulaire
+        sujet = form.cleaned_data['sujet']
+        message = form.cleaned_data['message']
+        envoyeur = form.cleaned_data['envoyeur']
+        renvoi = form.cleaned_data['renvoi']
+
+        # Nous pourrions ici envoyer l'e-mail grâce aux données 
+        # que nous venons de récupérer
+        envoi = True
+    
+    # Quoiqu'il arrive, on affiche la page du formulaire.
+    return render(request, 'blog/contact.html', locals())
